33a889dfdf94abd669081a4d700fe2520e5c7749
TBM13 <mateotrueba@gmail.com>
2024-03-03 13:38:49 -0300

    Support Wavebox (#12)

--- BrowserSearch/Main.cs
+++ BrowserSearch/Main.cs
@@ -160,6 +160,11 @@ namespace Community.Powertoys.Run.Plugin.BrowserSearch
                         Path.Join(localappdata, @"Vivaldi\User Data"), _selectedProfileName
                     );
                     break;
+                case "Wavebox":
+                    _defaultBrowser = new Chromium(
+                        Path.Join(localappdata, @"WaveboxApp\User Data"), _selectedProfileName
+                    );
+                    break;
                 default:
                     Log.Error($"Unsupported/unrecognized default browser '{BrowserInfo.Name}'", typeof(Main));
                     MessageBox.Show($"Browser '{BrowserInfo.Name}' is not supported", "BrowserSearch");
