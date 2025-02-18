32b1280565480763d125ae07c38a603b1be8b5e8
TBM13 <mateotrueba@gmail.com>
2025-02-18 15:58:18 -0300

    Upgrade to .NET 9

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 
 dotnet build %cd%\BrowserSearch.sln /target:BrowserSearch /property:GenerateFullPaths=true /consoleloggerparameters:NoSummary /p:Configuration=Release /p:Platform="x64"
 
-set "source=%cd%\BrowserSearch\bin\x64\Release\net8.0-windows"
+set "source=%cd%\BrowserSearch\bin\x64\Release\net9.0-windows"
 set "destination=%LOCALAPPDATA%\Microsoft\PowerToys\PowerToys Run\Plugins\BrowserSearch"
 
 if exist "%destination%" (
